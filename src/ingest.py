afeaa5b6e261e4ed1f6619e4579e3d33489feb0f
JVFayad <joao.victor.fa2014@gmail.com>
2026-03-10 20:16:11 -0300

    Add ingest code

--- src/ingest.py
+++ src/ingest.py
@@ -1,12 +1,49 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    for k in ("DATABASE_URL", "PG_VECTOR_COLLECTION_NAME", "GOOGLE_API_KEY", "GOOGLE_EMBEDDING_MODEL"):
+        if k not in os.environ:
+            raise RuntimeError(f"Missing required environment variable: {k}")
+
+    docs = PyPDFLoader(str(PDF_PATH)).load()
+
+    splits = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150, add_start_index=False).split_documents(docs)
+
+    if not splits:
+        raise SystemExit("No documents to process after splitting.")
+
+    enriched = [
+        Document(
+            page_content=doc.page_content,
+            metadata={k: v for k, v in doc.metadata.items() if v not in ("", None)},
+        )
+        for doc in splits
+    ]
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True
+    )
+
+    store.add_documents(enriched, ids=ids)
 
 
 if __name__ == "__main__":
