00cd31a35bdf285fb5b47408a22b254c38410e00
JVFayad <joao.victor.fa2014@gmail.com>
2026-03-10 21:13:01 -0300

    Ad search code

--- src/search.py
+++ src/search.py
@@ -1,6 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+load_dotenv()
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
-{contexto}
+{context}
 
 REGRAS:
 - Responda somente com base no CONTEXTO.
@@ -20,10 +30,38 @@ Pergunta: "Você acha isso bom ou ruim?"
 Resposta: "Não tenho informações necessárias para responder sua pergunta."
 
 PERGUNTA DO USUÁRIO:
-{pergunta}
+{question}
 
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
+
 def search_prompt(question=None):
-    pass
+    for k in ("DATABASE_URL", "PG_VECTOR_COLLECTION_NAME", "GOOGLE_API_KEY", "GOOGLE_EMBEDDING_MODEL"):
+        if k not in os.environ:
+            raise RuntimeError(f"Missing required environment variable: {k}")
+        
+    embeddings  = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_API_MODEL", "gemini-embedding-2-preview"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True
+    )   
+
+    database_context = store.similarity_search_with_score(question, k=10)
+
+    question_template = PromptTemplate(
+        input_variables=["context", "question"],
+        template=PROMPT_TEMPLATE
+    )
+
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0)
+ 
+    chain = question_template | llm
+
+    return chain.invoke({
+        "context": "\n\n".join([doc.page_content for doc, _ in database_context]),
+        "question": question
+    })
